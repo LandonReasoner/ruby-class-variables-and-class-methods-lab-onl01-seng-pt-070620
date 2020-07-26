5e7c02e43de4c1e192b32bed7c7543ab9b4a6ea3
Landon Reasoner <reasoner.landon90@gmail.com>
2020-07-26 22:41:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,17 +27,7 @@ class Song
    @@artists.uniq 
  end
  
- def self.genre_count
-   genre_count = {}
-   @@genres.each do |genre|
-     if genre_count[genre]
-       genre_count += 1
-     else
-       genre_count = 1 
-     end
-   end
-   genre_count
-  end
+ 
 end
 
 
